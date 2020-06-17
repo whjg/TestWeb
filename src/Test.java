fea0ec73616c8cfc96dc4b03e790c1f5f021c868
计刚 <whjg@yonyou.com>
2020-06-17 08:06:07 +0800

    第一次提交

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println();
+
+           System.out.println();
+        System.out.println();
+
+             String abc = new String();
+    }
+
+    /**
+     *
+     */
+    static void init() {
+
+    }
+}
